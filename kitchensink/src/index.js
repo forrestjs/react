157718e70d743fa69ea2fafad12b1702184dd38a
marco pegoraro <marco.pegoraro@gmail.com>
2021-06-13 13:18:35 +0200

    Add kitchensink

---
+++ kitchensink/src/index.js
@@ -0,0 +1,19 @@
+import { runHookApp } from '@forrestjs/hooks';
+
+// Services
+import reactRoot from '@forrestjs/react-root';
+import reactMUI from '@forrestjs/react-mui';
+import reactRouter from '@forrestjs/react-router';
+
+// Features
+import { muiTheme } from './features/mui-theme';
+import { layout } from './features/layout';
+import { dashboard } from './features/dashboard';
+import { users } from './features/users';
+import { invoices } from './features/invoices';
+
+runHookApp({
+  trace: 'compact',
+  services: [reactRoot, reactMUI, reactRouter],
+  features: [muiTheme, layout, dashboard, users, invoices],
+}).catch((err) => console.error(`Boot: ${err.message}`));
