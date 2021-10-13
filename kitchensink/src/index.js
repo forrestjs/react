0485e0b0bb23035e7490bd0cfbb43aa55f17c746
marcopeg <marco.pegoraro@gmail.com>
2021-10-13 18:40:25 +0200

    Update react-mui

--- kitchensink/src/index.js
+++ kitchensink/src/index.js
@@ -2,19 +2,24 @@ import { runHookApp } from '@forrestjs/hooks';
 
 // Services
 import reactRoot from '@forrestjs/react-root';
-import reactMUI from '@forrestjs/react-mui';
-import reactRouter from '@forrestjs/react-router';
-import reactAxios from './react-axios';
+// import reactMUI from '@forrestjs/react-mui';
+// import reactRouter from '@forrestjs/react-router';
+// import reactAxios from './react-axios';
 
 // Features
-import { muiTheme } from './features/mui-theme';
-import { layout } from './features/layout';
-import { dashboard } from './features/dashboard';
-import { users } from './features/users';
-import { invoices } from './features/invoices';
+// import { muiTheme } from './features/mui-theme';
+// import { layout } from './features/layout';
+// import { dashboard } from './features/dashboard';
+// import { users } from './features/users';
+// import { invoices } from './features/invoices';
 
 runHookApp({
   trace: 'compact',
-  services: [reactRoot, reactMUI, reactRouter, reactAxios],
-  features: [muiTheme, layout, dashboard, users, invoices],
+  services: [
+    reactRoot,
+    // reactMUI, reactRouter, reactAxios
+  ],
+  features: [
+    // muiTheme, layout, dashboard, users, invoices
+  ],
 }).catch((err) => console.error(`Boot: ${err.message}`));
