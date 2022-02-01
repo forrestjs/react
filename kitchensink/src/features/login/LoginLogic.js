1c2e7087d3c989032aab19ddc2fc8ef73d930a95
marcopeg <marco.pegoraro@gmail.com>
2022-02-01 09:19:48 +0100

    Update kitchensink

--- kitchensink/src/features/login/LoginLogic.js
+++ kitchensink/src/features/login/LoginLogic.js
@@ -1,12 +1,15 @@
-// import { useLogin } from "./LoginContext";
-import { useState } from 'react';
+import React from 'react';
+import { useLogin } from './LoginContext';
+import { LoginForm } from './LoginForm';
 
-const LoginLogic = () => {
-  // const login = useLogin();
-  // console.log(login);
-  useState('fo');
+export const LoginLogic = ({ root }) => {
+  const { user, login } = useLogin();
 
-  return <div>login</div>;
+  // Blocking UI to login the user
+  if (!user) {
+    return <LoginForm login={login} />;
+  }
+
+  // Render the received component
+  return React.createElement(root.component, root.props);
 };
-
-export default LoginLogic;
--- kitchensink/src/features/login/index.js
+++ kitchensink/src/features/login/index.js
@@ -1,13 +1,14 @@
-import LoginLogic from './LoginLogic';
-// import { LoginWrapper } from "./LoginContext";
+import { LoginLogic } from './LoginLogic';
+import { LoginWrapper } from './LoginContext';
 
 export const login = ({ registerAction }) => {
   registerAction({
     hook: '$REACT_ROOT_COMPONENT',
-    handler: () => <LoginLogic />,
+    handler: (root) => ({ component: LoginLogic, props: { root } }),
   });
-  // registerAction({
-  //   hook: "$REACT_ROOT_WRAPPER",
-  //   handler: LoginWrapper
-  // });
+
+  registerAction({
+    hook: '$REACT_ROOT_WRAPPER',
+    handler: { component: LoginWrapper },
+  });
 };
