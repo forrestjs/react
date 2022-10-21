ff3fef4307630231d88447bc72e34eb6abbc5dab
marcopeg <marco.pegoraro@gmail.com>
2022-10-21 09:10:53 +0200

    Add types to hooks

--- packages/react-root/src/forrestjs-provider.js
+++ packages/react-root/src/forrestjs-provider.js
@@ -2,14 +2,26 @@ import React, { useContext } from 'react';
 
 export const ForrestJSContext = React.createContext();
 
-export const useForrestJS = (path, defaultValue) =>
-  useContext(ForrestJSContext);
+/**
+ * @returns {any} ForrestJS Context
+ */
+export const useForrestJS = () => useContext(ForrestJSContext);
 
+/**
+ * @param {String} path
+ * @param {any} [defaultValue]
+ * @returns {any}
+ */
 export const useGetConfig = (path, defaultValue) => {
   const { getConfig } = useContext(ForrestJSContext);
   return getConfig(path, defaultValue);
 };
 
+/**
+ * @param {String} path
+ * @param {any} [defaultValue]
+ * @returns {any}
+ */
 export const useGetContext = (path, defaultValue) => {
   const { getContext } = useContext(ForrestJSContext);
   return getContext(path, defaultValue);
